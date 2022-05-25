194c60fa67028836009ac562c53a8ba9e3cdd5a1
Stefan Milanovic <stefan.fva@gmail.com>
2022-05-25 18:37:00 -0400

    Built out most of the page sections and started populating them with content.

--- PortfolioWebsite/Client/Program.cs
+++ PortfolioWebsite/Client/Program.cs
@@ -1,3 +1,4 @@
+using Majorsoft.Blazor.Components.Common.JsInterop;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PortfolioWebsite.Client;
@@ -7,5 +8,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+//Register dependencies
+builder.Services.AddJsInteropExtensions();
 
 await builder.Build().RunAsync();
